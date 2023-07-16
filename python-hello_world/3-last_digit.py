7556fc94aaf60295b4fafbf6b32dc6980eaaa42e
HMDT2023 <merveilhouenagnon@gmail.com>
2023-07-16 22:45:11 +0100

    3-last_digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,7 +10,7 @@ if last_digit > 5:
 elif last_digit == 0:
     message = "and is 0"
 else:
-    message = "and is less than 5 and not 0"
+    message = "and is less than 6 and not 0"
 
 print("Last digit of", number, "is", last_digit, message)
 
