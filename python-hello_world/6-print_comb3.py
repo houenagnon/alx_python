fd120933a0ffef6e89be852e6577dff845b48fc0
HMDT2023 <merveilhouenagnon@gmail.com>
2023-07-19 13:55:46 +0100

    a

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 for i in range(10):
-    for j in range(10):
-        if i != j:
-            print("{0}{1}".format(i, j))
+    for j in range(i + 1, 10):
+        print("{:d}".format(i) + "{:d}".format(j), end=", " if i < 8 or j < 9 else "\n")
+
 
 
