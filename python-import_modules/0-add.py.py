e105c50b2fe76dff559e85547ec8cd7886569dfc
HMDT2023 <merveilhouenagnon@gmail.com>
2023-07-26 17:56:38 +0100

    A

--- python-import_modules/0-add.py.py
+++ python-import_modules/0-add.py.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
+import add_0
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+a = 1
+b = 2
+print(f"{a} + {b} = {add_0.add(a, b)}")
 
