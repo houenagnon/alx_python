a7046ae59d9254303b480b578e7e985943646430
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-01 15:55:31 +0100

    a

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,66 +1,17 @@
 #!/usr/bin/python3
-"""Module which contains BaseGeometry class
-"""
+'''Class Rectangle that inherits from BaseGeometry'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
-class BaseGeometry:
-    """class BaseGeometry that is empty"""
-
-    def area(self):
-        """function that raises an exception
-        Args:
-            self : the object
-        Returns:
-            void
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """function that validates value
-        Args:
-            self : the object
-            name (string): the name
-            value (int): the value
-        Returns:
-            void
-        """
-        if not isinstance(value, int):
-            temp = name + " must be an integer"
-            raise TypeError(temp)
-
-        if value <= 0:
-            temp = name + " must be greater than 0"
-            raise TypeError(temp)
 
 class Rectangle(BaseGeometry):
-    """class Rectangle that is not empty"""
-
+    '''Class Rectangle'''
     def __init__(self, width, height):
-        """function that initializes attributes
+        '''Initialization
         Args:
-            self : the object
-            width : the width
-            height : the height
-        Returns:
-            void
-        """
+            width (int): width of rectangle
+            height (int): height og rectangle
+        '''
         super().integer_validator("width", width)
-        self.__width = width
         super().integer_validator("height", height)
+        self.__width = width
         self.__height = height
-
-
-r = Rectangle(3, 5)
-
-print(r)
-print(dir(r))
-
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
