a6f28373e5da86e6016b7bdef597486d3e2a371f
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-01 09:02:19 +0100

    all

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
+    
     def safe_print_division(a, b):
-
-        try: 
-            result = int(a)/int(b)
+        try:
+            result = a / b
         except ZeroDivisionError:
             result = None
-        finally: 
-            print("{:d} / {:d} = {}".format(a, b, result)) 
+        finally:
+            print("Inside result: {}".format(result))
+
+        return result 
--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -3,4 +3,5 @@
 if __name__ == "__main__":
 
     def raise_exception():
+        
         raise TypeError
