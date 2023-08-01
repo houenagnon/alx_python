1732b5b09b7ea04e68305ba99ff3efc2e5477437
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-01 04:14:19 +0100

    safe_print_division

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    def safe_print_division(a, b):
+
+        try : 
+            a = int(a)
+            b = int(b)
+            result = a/b
+        except ZeroDivisionError :
+            result = None
+        except ValueError : 
+            result = None
+        finally : 
+            print("{:d} / {:d} = {}".format(a, b, result)) 
