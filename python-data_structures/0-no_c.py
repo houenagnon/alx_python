94e72ee45ed1d4a68994339dd6c3399094097f30
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-01 09:31:07 +0100

    all

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+
+    if 'c' not in my_string and 'C' not in my_string:
+        return my_string
+
+    new_string = ""
+    for char in my_string:
+        if char not in "Cc":
+            new_string += char
+    return new_string
