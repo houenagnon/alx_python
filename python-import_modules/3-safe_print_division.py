5d5ff1f8eccb3539e528476829f2c08dbba04179
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-01 08:22:34 +0100

    a

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,9 +5,7 @@ if __name__ == "__main__":
     def safe_print_division(a, b):
 
         try: 
-            a = int(a)
-            b = int(b)
-            result = a/b
+            result = int(a)/int(b)
         except ZeroDivisionError:
             result = None
         finally: 
