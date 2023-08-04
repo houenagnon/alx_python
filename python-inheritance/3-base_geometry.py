930e32abae9e7842a6c1ce522212c8a8d845b5a0
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-04 03:41:02 +0100

    az

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -9,4 +9,3 @@ class BaseGeometry:
         #get list of all attributes for this class and exclude __init_subclass
         attributes = super().__dir__()
         return[attribute for attribute in attributes if attribute != '__init_subclass__']
-    pass
