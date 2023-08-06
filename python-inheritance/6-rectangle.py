3f93bca200cd1d2aa2a3e99151e859bc1ab1e002
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-06 12:58:06 +0100

    mm

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -3,34 +3,45 @@
 """
 
 
-class BaseGeometry:
-    """class BaseGeometry that is empty"""
+class Base(type):
+    """
+    This class is write to exclude __init_subclass__
+    """
 
+    def __dir__(cls) -> None:
+        #get list of all attributes for this class and exclude __init_subclass
+        
+        attributes = super().__dir__()
+
+        list_to_return = []
+        for attr in attributes:
+            if attr != "__init_subclass__":
+                list_to_return.append(attr)
+        return list_to_return
+    
+class BaseGeometry(metaclass=Base):
+    '''Class BaseGeometry'''
+    def __dir__(cls):
+        #get list of all attributes for this class and exclude __init_subclass
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
     def area(self):
-        """function that raises an exception
-        Args:
-            self : the object
-        Returns:
-            void
-        """
+        '''Signals that area is not implemented'''
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """function that validates value
+        '''validates the value of name
         Args:
-            self : the object
-            name (string): the name
-            value (int): the value
-        Returns:
-            void
-        """
-        if not isinstance(value, int):
-            temp = name + " must be an integer"
-            raise TypeError(temp)
-
+            name (str): the value given
+            value (int): the value of name
+        Exception:
+            TypeError if value is not an integer
+            ValueError if value is less or equal than 0'''
+        
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            temp = name + " must be greater than 0"
-            raise TypeError(temp)
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """class Rectangle that is not empty"""
@@ -48,19 +59,3 @@ class Rectangle(BaseGeometry):
         self.__width = width
         super().integer_validator("height", height)
         self.__height = height
-
-
-r = Rectangle(3, 5)
-
-print(r)
-print(dir(r))
-
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
