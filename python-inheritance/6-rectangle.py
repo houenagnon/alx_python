c9c45daa20c127a4677766e12fac5c091017e292
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-06 13:00:13 +0100

    zz

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -43,7 +43,7 @@ class BaseGeometry(metaclass=Base):
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
-class Rectangle(BaseGeometry):
+class Rectangle(metaclass=BaseGeometry):
     """class Rectangle that is not empty"""
 
     def __init__(self, width, height):
