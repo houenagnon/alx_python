347cb50a82aad331f90d05f7c4695551d3cf8faf
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-06 12:06:46 +0100

    pp

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,6 +2,9 @@
 
 '''Module that defines a class named BaseGeometry'''
 
+from collections.abc import Iterable
+
+
 class Base(type):
     """
     This class is write to exclude __init_subclass__
@@ -21,6 +24,9 @@ class Base(type):
 
 class BaseGeometry(metaclass=Base):
     '''Class BaseGeometry'''
+
+    def __dir__(cls):
+        #get list of all attributes for this class and exclude __init_subclass
+        return [attribute for attribute in super.__dir__() if attribute != '__init_subclass__']
     
-   
     pass
--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -2,9 +2,30 @@
 
 '''Module that defines a class named BaseGeometry'''
 
-class BaseGeometry:
+class Base(type):
+    """
+    This class is write to exclude __init_subclass__
+    """
+
+    def __dir__(cls) -> None:
+        #get list of all attributes for this class and exclude __init_subclass
+        
+        attributes = super().__dir__()
+
+        list_to_return = []
+        for attr in attributes:
+            if attr != "__init_subclass__":
+                list_to_return.append(attr)
+        return list_to_return
+
+
+class BaseGeometry(metaclass=Base):
     '''Class BaseGeometry'''
     
     def area(self):
         '''Signals that area is not implemented'''
         raise Exception("area() is not implemented")
+    
+    def __dir__(cls):
+        #get list of all attributes for this class and exclude __init_subclass
+        return [attribute for attribute in super.__dir__() if attribute != '__init_subclass__']
