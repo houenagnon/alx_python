ec4376b0bb9bad6815516c5b0c89e9ee4d470e0c
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-07 19:27:06 +0100

    a

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,45 +1,46 @@
-"""Module which contains Rectangle class
+#!/usr/bin/python3
+"""Module which contains BaseGeometry class
 """
 
-# class Base(type):
-#     """
-#     This class is write to exclude __init_subclass__
-#     """
+class Base(type):
+    """
+    This class is write to exclude __init_subclass__
+    """
 
-#     def __dir__(cls) -> None:
-#         #get list of all attributes for this class and exclude __init_subclass
+    def __dir__(cls) -> None:
+        #get list of all attributes for this class and exclude __init_subclass
         
-#         attributes = super().__dir__()
+        attributes = super().__dir__()
 
-#         list_to_return = []
-#         for attr in attributes:
-#             if attr != "__init_subclass__":
-#                 list_to_return.append(attr)
-#         return list_to_return
+        list_to_return = []
+        for attr in attributes:
+            if attr != "__init_subclass__":
+                list_to_return.append(attr)
+        return list_to_return
     
-# class BaseGeometry(metaclass=Base):
-#     '''Class BaseGeometry'''
-#     def __dir__(cls):
-#         #get list of all attributes for this class and exclude __init_subclass
-#         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class BaseGeometry(metaclass=Base):
+    '''Class BaseGeometry'''
+    def __dir__(cls):
+        #get list of all attributes for this class and exclude __init_subclass
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     
-#     def area(self):
-#         '''Signals that area is not implemented'''
-#         raise Exception("area() is not implemented")
+    def area(self):
+        '''Signals that area is not implemented'''
+        raise Exception("area() is not implemented")
 
-#     def integer_validator(self, name, value):
-#         '''validates the value of name
-#         Args:
-#             name (str): the value given
-#             value (int): the value of name
-#         Exception:
-#             TypeError if value is not an integer
-#             ValueError if value is less or equal than 0'''
+    def integer_validator(self, name, value):
+        '''validates the value of name
+        Args:
+            name (str): the value given
+            value (int): the value of name
+        Exception:
+            TypeError if value is not an integer
+            ValueError if value is less or equal than 0'''
         
-#         if type(value) != int:
-#             raise TypeError("{} must be an integer".format(name))
-#         if value <= 0:
-#             raise ValueError("{} must be greater than 0".format(name))
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """class Rectangle that is not empty"""
