956d38d2caa370f9ea30f2f97bb60bcc2eca5b1a
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-08 22:09:42 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,7 +6,9 @@ class Square(Rectangle):
     """This class inherit from Rectangle Class"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """This is the init method of the class"""
         super().__init__(size, size, x, y, id)
     
     def __str__(self):
+        """This method is to print the information about the class"""
         return("[Rectangle] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
