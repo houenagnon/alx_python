085a8d537e669fcdaf7701a47cc849a3618d5f4c
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-08 23:54:52 +0100

    0-hbtn

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+
+req = requests.get("https://alu-intranet.hbtn.io/status")
+
+print("Body response:")
+print(end=" ")
+print("- type:", req.text)
+print(end=" ")
+print("- content:", type(req.text))             
