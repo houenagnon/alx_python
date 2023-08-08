21ffa5d314816858bc89c8edf60fb245d066c960
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-09 00:05:56 +0100

    rq

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,7 +4,5 @@ import requests
 req = requests.get("https://alu-intranet.hbtn.io/status")
 
 print("Body response:")
-print(end="       ")
-print("- type:", type(req.text))
-print(end="      ")
-print("- content:", req.text )             
+print("\t- type:", type(req.text))
+print("\t- content:", req.text )             
