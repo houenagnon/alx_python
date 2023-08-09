8d4e1abde65fb544ee39ff725b6aa7f1c8837335
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-09 11:09:06 +0100

    aqs

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,10 +1,19 @@
-#!/usr/bin/python3
-#Use X-Resquest-Id in headers response"""
+#!/usr/bin/env python3
+"""
+This script sends a request to a given URL and displays the value of the X-Request-Id header.
+Usage: python script.py <URL>
+"""
+
 import requests
 import sys
 
-#Make the server request using get method
-req = requests.get(sys.argv[1])
+# Get the URL from the command-line argument
+url = sys.argv[1]
 
-#Print of X-Request-Id attribute
-print(req.headers['X-Request-Id'])
+# Send a GET request to the URL
+response = requests.get(url)
+
+# Display the X-Request-Id header if it exists
+x_request_id = response.headers.get("X-Request-Id")
+
+print(x_request_id)
