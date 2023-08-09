592e8dac30a08fc4738dfc67aebbaca18961c793
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-09 11:02:58 +0100

    wx

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-#Use X-Resquest-Id in headers response
 import requests
 import sys
 
-#Make the server request using get method
 req = requests.get(sys.argv[1])
 
-#Print of X-Request-Id attribute
 print(req.headers['X-Request-Id'])
