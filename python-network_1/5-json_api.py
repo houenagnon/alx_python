ada5d0532b9414d67d30d9d514cdbcd9779f21f1
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-10 23:36:43 +0100

    az

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -7,16 +7,26 @@ Usage: python script.py <URL>
 import requests
 import sys
 
-# Get the letter from the command-line arguments
-q = sys.argv[1]
+def search_user_by_letter(letter):
+    url = "http://0.0.0.0:5000/search_user"
+    params = {'q': letter}
+    response = requests.post(url, data=params)
 
-if q == " ":
-    q = " "
+    try:
+        response_json = response.json()
+        if response_json:
+            user_id = response_json.get('id')
+            user_name = response_json.get('name')
+            print(f"[{user_id}] {user_name}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
 
-# Send a POST request 
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
 
-response = requests.post("http://0.0.0.0:5000/search_user", data=q)
-
-
-# Display the body of the response
-print(response.text)
+    search_user_by_letter(letter)
