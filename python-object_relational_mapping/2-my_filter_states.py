7ba6a06336ee859a8faaa3fa69d2ab7dc876f6f7
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-21 02:43:04 +0100

    a

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,8 +3,8 @@ states table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 import sys
-
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], 
+                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 cur = conn.cursor()
 cur.execute(("SELECT * FROM states WHERE name = %s ORDER BY id ASC "), (sys.argv[4],))
 query_rows = cur.fetchall()
