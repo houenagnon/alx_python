ea7162cb23e92e4cab53968cbb6531358e8b05eb
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-31 20:35:15 +0100

    az

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -26,7 +26,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def display(text):
-    return "c " + text.replace("_", " ")
+    return "C " + text.replace("_", " ")
 
 if __name__ == "__main__":
     app.run(debug='True', port=5000, host='0.0.0.0')
