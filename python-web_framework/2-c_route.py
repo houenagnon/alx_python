e5585b112e08376027ba1b1c713cbf5e212e2ed2
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-31 20:25:39 +0100

    aa

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -1,32 +1,24 @@
 #! /usr/bin/python3
-
 """
-    Your web application must be listening on 0.0.0.0, port 5000
-    Routes:
-        /: display “Hello HBNB!”
-        /hbnb: display “HBNB”
-        /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
-    You must use the option strict_slashes=False in your route definition
-
+    python flask basic server
 """
-
 from flask import Flask
-
 app = Flask(__name__)
-
 
 
 @app.route("/", strict_slashes=False)
 def home():
+    """return string"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
 @app.route("/c/<text>", strict_slashes=False)
-def display(text):
+def hbnb(text):
     return "c {}".format(text.split("_").join(" "))
 
 if __name__ == "__main__":
-    app.run(debug='True', port=5000, host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0", port="5000")
