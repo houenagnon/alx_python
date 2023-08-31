078ddc9af6724a6166687c6bfd0e72900a0f58b3
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-31 19:56:47 +0100

    all

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,7 +1,11 @@
 #! /usr/bin/python3
 
 """
-    Basic flask web server
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+    You must use the option strict_slashes=False in your route definition
 """
 
 from flask import Flask
@@ -15,7 +19,8 @@ def home():
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
-def home():
+def hbnb():
+    #route for /hbnb
     return "HBNB"
 
 if __name__ == "__main__":
--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -1,7 +1,13 @@
 #! /usr/bin/python3
 
 """
-    Basic flask web server
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+    You must use the option strict_slashes=False in your route definition
+
 """
 
 from flask import Flask
